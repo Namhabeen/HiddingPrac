a152a8aadbfc9b5ece01f2939e0c0fdeb86aa5aa
NAMHABEEN <s2018s27@e-mirim.hs.kr>
2020-06-16 14:44:58 +0900

    add method, value

--- HiddingPrac/Program.cs
+++ HiddingPrac/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HiddingPrac
+namespace HidingPrac
 {
     class Program
     {
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모메서드");
+            }
+        }
+        
+        class Child : Parent
+        {
+            public string variable = "Hiding";
+            public void Method()
+            {
+                Console.WriteLine("자식메서드");
+            }
+        }
         static void Main(string[] args)
         {
+            Child c = new Child();
+            Console.WriteLine("c = " + c.variable);
+            c.Method();
+            Console.WriteLine("부모취급 c = " + ((Parent)c).variable);
+            ((Parent)c).Method();
         }
     }
 }
